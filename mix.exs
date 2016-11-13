6254057ce6cfab820b64a838f077e8dcd2e5c3cb
Joshua Philips <jmichaelphilips@gmail.com>
2016-11-12 22:07:39 -0600

    updated the mix.exs file

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Slackdice.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger, :trot]]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +27,9 @@ defmodule Slackdice.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:trot, github: "hexedpackets/trot"},
+      {:poison, "~> 1.4"}
+    ]
   end
 end
